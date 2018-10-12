2a6f459a63f0e0bd4dd2f2ada64b94b8ebea6229
Klaudia <klaudia.pawelek@cranfield.ac.uk>
2018-10-12 16:18:34 +0100

    Sort class

---
+++ SortingLibrary/SortingLibrary/Sort.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <cstdlib> // for the functions rand, srand
+#include <ctime> // for the function time
+#include "Sort.h"
+
+
+// default constructor, empty vector
+Sort::Sort()
+{
+	vector<int>();
+}
+
+//additional constructor, fill vector by random numbers
+Sort::Sort(int sizeOfVector)
+{
+	// set the size
+	this->vectorOfSortedValues=vector<int>(sizeOfVector);
+
+	//random numbers
+	srand(time(NULL));
+	for (int i = 1; i <= sizeOfVector; i++)
+	{
+		int rnum = rand() % 100 + 1; //the range 1 to 100
+		this->vectorOfSortedValues[sizeOfVector - i] = rnum;
+	}
+
+}
+
+//method can be used to print sorted and unsorted vector
+void Sort::PrintVector()
+{
+	//todo
+}
+
+// method for sorting algorithm from first group: insertion sort or bubble sort;
+void Sort::sortAlgorithm1()
+{
+	//todo
+}
+
+// method for sorting algorithm from first group: merge sort/quicksort/radix list sort/heapsort
+void Sort::sortAlgorithm2()
+{
+	//todo
+}
+
+
+Sort::~Sort()
+{
+	//todo
+}
--- SortingLibrary/SortingLibrary/maincpp.cpp
+++ SortingLibrary/SortingLibrary/maincpp.cpp
@@ -1,1 +1,19 @@
-//test
+#include <iostream>
+#include <istream>
+#include <vector>
+#include "Sort.h"
+using namespace std;
+
+int main()
+{
+	//ask user
+	int sizeOfVector = 0;
+	cout << "Insert size of vector, which will be contained random values: ";
+	cin >> sizeOfVector;
+
+	Sort s(sizeOfVector);
+
+
+	cout << endl;
+	return 0;
+}
